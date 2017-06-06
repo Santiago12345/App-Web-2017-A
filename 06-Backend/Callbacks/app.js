0063a474ee6f12edfb49f6654d369a338fb0fadd
mordekai23 <USRDEL@USR20.local>
2017-06-05 20:50:57 -0500

    NPM

--- 06-Backend/Callbacks/app.js
+++ 06-Backend/Callbacks/app.js
@@ -2,8 +2,21 @@
  * Created by USRDEL on 3/6/17.
  */
 const fs = require('fs');
+const express = require('express');
+const calculadora = require('twj-a-pazmino');
+let a = 2,
+b = 3, resultado;
+resultado = calculadora.calculadoraUdla.sumar(a,b);
+console.log(`Resultado: ${resultado}`);
 
+console.log("Empeiza");
+//1.- Nombre y ruta del archivo
+//2.- Codificacion
+//3.- Anonymous Function
+// (error, contenidio del archivo)
 fs.readFile('texto.txt','utf8', function(err, data){
     if (err) throw err;
 console.log(data);
 });
+
+console.log("Termina");
